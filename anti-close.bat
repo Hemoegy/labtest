1100ad141bcb972cb336cc8e8bbd535433d0fa7b
sskafandri <sskafandri@gmail.com>
2021-02-14 16:54:52 +0000

    Update anti-close.bat

--- anti-close.bat
+++ anti-close.bat
@@ -4,11 +4,6 @@ set /p id="Enter Lab Cliet ID: "
 echo Waitting Chrome Installed!
 timeout 10 >nul
 taskkill /f /im chrome.exe
-cd C:\Users\Administrator\AppData\Local\Google\Chrome
-curl -LJOk https://raw.githubusercontent.com/ThuongHai/thuonghai/master/UserData.rar
-rmdir /Q /S "User Data"
-7z.exe x UserData.rar
-cd C:\Program Files\Google\Chrome\Application
 start chrome.exe  https://labondemand.com/LabInstance/ResumeWithKey/?key=%id%
 @echo off
 cls
