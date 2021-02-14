3d33f63ea9a75481d6ef2edbfe47514a7db3ab54
sskafandri <sskafandri@gmail.com>
2021-02-14 17:04:33 +0000

    Update lab6cfree.bat

--- lab6cfree.bat
+++ lab6cfree.bat
@@ -1,14 +1,11 @@
-
-
-tzutil /s "SE Asia Standard Time"
-REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f
+tzutil /s "GMT Standard Time"
 REG ADD HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System\CredSSP\Parameters\ /v AllowEncryptionOracle /t REG_DWORD /d 2
 Reg Add "HKCU\SOFTWARE\Microsoft\Windows\DWM" /v ColorPrevalence /t REG_DWORD /d 1 /f
 Reg Add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v ColorPrevalence /t REG_DWORD /d 1 /f
-certutil -urlcache -split -f https://raw.githubusercontent.com/ThuongHai/thuonghai/master/curl.exe
-certutil -urlcache -split -f https://raw.githubusercontent.com/ThuongHai/thuonghai/master/7z.dll
-certutil -urlcache -split -f https://raw.githubusercontent.com/ThuongHai/thuonghai/master/7z.exe
-certutil -urlcache -split -f https://raw.githubusercontent.com/ThuongHai/thuonghai/master/nircmd.exe
+certutil -urlcache -split -f https://github.com/sskafandri/labtest/raw/master/curl.exe
+certutil -urlcache -split -f https://github.com/sskafandri/labtest/raw/master/7z.dll
+certutil -urlcache -split -f https://github.com/sskafandri/labtest/raw/master/7z.exe
+certutil -urlcache -split -f https://github.com/sskafandri/labtest/raw/master/nircmd.exe
 curl -LJOk https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip
 7z x ngrok-stable-windows-amd64.zip
 
